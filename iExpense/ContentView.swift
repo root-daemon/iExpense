425a6b413b05e1b46094f332f0a782be0ec4a30b
root_daemon <srivishal.sivasubramanian@gmail.com>
2024-11-05 12:19:23 +0530

    Add validation to AddView fields

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -172,3 +172,4 @@ struct ContentView_Previews: PreviewProvider {
 // Change 75: Sun Apr 27 13:46:32 IST 2025
 // Change 3: Sun Apr 27 13:47:39 IST 2025
 // Change 5: Sun Apr 27 13:47:40 IST 2025
+// Change 8: Sun Apr 27 13:47:41 IST 2025
