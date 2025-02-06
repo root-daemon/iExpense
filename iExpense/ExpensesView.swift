2f7c74ac2bd9ee48699ceba46bcdc7a88872e4cc
root_daemon <srivishal.sivasubramanian@gmail.com>
2025-02-07 04:46:17 +0530

    Improve accessibility in ContentView

--- iExpense/ExpensesView.swift
+++ iExpense/ExpensesView.swift
@@ -114,3 +114,4 @@ struct ExpensesView: View {
 // Change 26: Sun Apr 27 13:47:55 IST 2025
 // Change 39: Sun Apr 27 13:48:02 IST 2025
 // Change 40: Sun Apr 27 13:48:03 IST 2025
+// Change 44: Sun Apr 27 13:48:06 IST 2025
